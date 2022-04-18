d04c99334e33116805c4e3ba1dfbe882272bb42d
ValenReynolds <valenryanreynolds@gmail.com>
2022-04-18 08:50:11 -0700

    force login for Fibonacci
    Signed-off-by: ValenReynolds <valenryanreynolds@gmail.com>

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,6 +1,37 @@
-from flask import Blueprint, render_template, request
+from __init__ import login_manager
+from flask import Blueprint, render_template, request, url_for, redirect, jsonify, make_response
+from flask_login import login_required
+
 from .fibonacci import Fibonacci
 from .palindrome import Palindrome
+
+# blueprint defaults https://flask.palletsprojects.com/en/2.0.x/api/#blueprint-objects
+app_fibonacci = Blueprint('crud', __name__,
+                     url_prefix='/crud',
+                     template_folder='templates/cruddy/:templates/:algorithms/templates/',
+                     static_folder='static',
+                     static_url_path='static')
+
+ 
+# If you decorate a view(route) with this, it will ensure that the current user is logged in and authenticated before calling the actual view. 
+# (If they are not, it calls the LoginManager.unauthorized callback.). 
+# Use this example for Hack #3.
+@app_fibonacci.route('/')
+@app_fibonacci.route('/palindrome/')
+@login_required  # Flask-Login uses this decorator to restrict access to logged in users
+def fibonacci():
+    """obtains all Users from table and loads Admin Form"""    
+    if request.form:
+        return render_template("fibonacci.html", fibonacci=Fibonacci(int(request.form.get("series"))))
+    return render_template("fibonacci.html", fibonacci=Fibonacci(2))
+
+# Unauthorised users do not get access to the Fibonacci
+# Flask-Login directs unauthorised users to this unauthorized_handler
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    return redirect(url_for('crud.crud_login'))
+
 
 app_algorithm = Blueprint('algorithm', __name__,
                           url_prefix='/algorithm',
